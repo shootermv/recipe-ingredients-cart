73ea5b9a571a4afe5083affb593a83f54f113e61
moshev <moshev@welldone-software.com>
2020-01-23 19:24:57 +0200

    Updates

---
+++ precache-manifest.54200f78b84e0851b9174e155d0d7b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b85882654e5b969960",
+    "url": "/recipe-ingredients-cart/static/css/main.4d8918f1.chunk.css"
+  },
+  {
+    "revision": "c2b85882654e5b969960",
+    "url": "/recipe-ingredients-cart/static/js/main.48f0167d.chunk.js"
+  },
+  {
+    "revision": "4adc9304f369838af2b9",
+    "url": "/recipe-ingredients-cart/static/js/runtime~main.1f3f1c4f.js"
+  },
+  {
+    "revision": "a55a0cb0e33bd3da337e",
+    "url": "/recipe-ingredients-cart/static/js/2.2a55f6fd.chunk.js"
+  },
+  {
+    "revision": "6c05eb07a7b9a2671d00559099ab27f6",
+    "url": "/recipe-ingredients-cart/static/media/beet.6c05eb07.jpeg"
+  },
+  {
+    "revision": "2d105b845016561cf50e1459296be2a1",
+    "url": "/recipe-ingredients-cart/index.html"
+  }
+];
